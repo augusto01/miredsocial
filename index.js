b41b09f5d538e40e97adaeb8415436df13dd8139
Almiron Augusto <almironaugusto404@gmail.com>
2024-12-26 10:20:14 -0300

    primera_ruta

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
+//dependencias 
+const express = require('express');
+const cors = require('cors');
+
 //conexion a la bd
 const {connection} = require('./database/connection');
-
-//conectar a la bd
 connection();
 
 
-//configurar cors
 
+//crear servidor node
+const app = express();
+const puerto = 3900;
+
+//configurar corse
+app.use(cors());
 
 //convertir datos del body a objetos json 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 //cargar conf rutas 
 
 
+//rutas
+
+app.get('/ruta_prueba', (req, res) =>{
+    return res.status(200).json({
+        mensaje: 'ruta de prueba'
+    });
+})
+
 //Poner servidor a escurchar peticiones http 
+
+app.listen(puerto, () =>{
+    console.log('Servidor corriendo en el puerto', puerto);
+});
